4bc1e3038da1bbc14e083ebba4a0076e6c1095e3
Insecurly <grstanko@s.sfusd.edu>
2023-10-17 16:53:54 -0700

    Co-authored-by: TheOtherTridentGuy <TheOtherTridentGuy@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,38 +1,43 @@
 # pylint: disable-all
 # Note that I'm using flask with async. Install with: pip install "Flask[async]"
-from quart import Quart, Request, render_template
+from quart import Quart, Request, render_template, abort
 import pyatv
 import asyncio
+import json
 
 
 app = Quart(__name__)
-
+cache = {}
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+async def index():
+    return await render_template("index.html")
 
-@app.route("/missioncontrol/<string:id>")
-async def mission_control(id):
-    pass
+@app.route("/control/<string:id>")
+async def control(id):
+    global cache
+    atv = cache.get(id)
+    if atv:
+        return await render_template("control.html", name=atv.name, data=str(atv).replace("\n", "<br>"))
+    return await render_template("error.html", textual="Apple TV", code=404)
 
 @app.errorhandler(404)
-def wrong_lever(e):
-    return render_template("text.html", text="Wrong URL Kronk! 404.")
+async def wrong_lever(e):
+    return await render_template("error.html", textual="URL", code=404)
 
 @app.route("/scan")
 async def scan():
+    global cache
     result_html = ""
     n = "\n"
     for result in await pyatv.scan(loop=asyncio.get_event_loop()):
+        cache[result.identifier] = result
         result_html += str(f"""
-<h3>{result.name}</h3>
-<br>
-<p>{str(result).replace(n, "<br>")}</p>
-<a href="/missioncontrol/{result.identifier}">Mission Control<a>
+<h3><a href="/control/{result.identifier}">{result.name}</a></h3>
+<p>{str(result).replace(n, "<br>")}</p><hr>
 """)
     if result_html:
-        return render_template("scanner.html", results=result_html)
+        return await render_template("scanner.html", results=result_html)
     return render_template("scanner.html", results="<h3>There are no Apple TVs on your network.</h3><a href='/'>Go Home</a>")
 
 app.run("localhost", 8080)
