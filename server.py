9605f388a083418664f98a44a0a061f48c6eb21d
insecurly <144853612+insecurly@users.noreply.github.com>
2023-11-14 14:32:09 -0800

    Move to raw aiohttp and add more functions

--- server.py
+++ server.py
@@ -1,91 +1,63 @@
-# pylint: disable-all
-from quart import Quart, Request, render_template
-from werkzeug.exceptions import HTTPException
-import pyatv
 import asyncio
-import json
-from random import randint
+from jinja2 import Template
+import os
+import pyatv
+
+routes = web.RouteTableDef()
+
+templateDir = "templates"
+staticDir = "static"
 
 
-
-app = Quart(__name__)
+templates = {}
 cache = {}
-authed_tvs = {}
-pairs_in_progress = {}
 
 
-@app.route("/")
-async def index():
-    return await render_template("index.html")
+def dumpFile(fileName): 
+    file = open(fileName, "r")
+    return file.read()
 
-@app.route("/beginpair/<string:id>")
-async def begin_pair(id):
-    global cache
-    tv = cache.get(str(id))
-    if tv:
-        pyatv
-        pair = await pyatv.pair(tv, pyatv.const.Protocol.AirPlay, asyncio.get_event_loop())
-        await pair.begin()
-        pairs_in_progress[id] = [pair, tv]
-        return "Done!", 200
-    return "TV Not in cache.", 404
+def html(text):
+    return web.Response(text=text, content_type='text/html')
 
-@app.route("/finishpair/<string:id>/<int:pin>")
-async def finish_pair(id, pin):
-    print("finish")
-    print(id, pin)
-    pair = pairs_in_progress.get(id)
-    print(pair)
-    if pair and pin:
-        print("pairing...")
-        pair[0].pin(pin) # index 0 is the pair object
-        await pair[0].finish()
-        authed_tvs[id] = pair[1] # index 1 is the tv itself
-        await pyatv.interface.Stream.play_url(pair[1], "damage.mp4")
-        return "Done!", 200
-    return "TV Has not yet started pair or you didn't provide a pin.", 404
+# Build templates from templates directory
+for filename in os.listdir(templateDir):
+    fullPath = os.path.join(templateDir, filename)
+    if os.path.isfile(fullPath):
+          templates[filename] = Template(dumpFile(fullPath))
+
+@routes.get('/style.css')
+async def css(request):
+    return web.Response(text=dumpFile("static/style.css"), content_type='text/css')
+
+@routes.get('/')
+async def index(request):
+    return web.Response(text="Hello, world!")
+
+@routes.get('/debug/{variable}')
+async def hello(request):
+    variable = request.match_info['variable']
+    print(eval(variable))
+    return web.Response(text="Printed to server console")
+
+@routes.get('/renderTemplate')
+async def render(request):
+
+    return html(templates["index.html"].render(name="d"))
 
 
-@app.errorhandler(Exception)
-async def wrong_lever(e):
-    errors = {
-        "400": "request",
-        "401": "user",
-        "402": "price",
-        "403": "permissions",
-        "404": "url",
-        "405": "method",
-        "407": "proxy",
-        "415": "media type",
-        "418": "pot type",
-        "451": "subpoena"
-    }
-    if isinstance(e, HTTPException):
-        print()
-        return await render_template("error.html", textual=errors.get(str(e.code)), code=e.code)
-    print(e)
-    return await render_template("error.html", textual="server code", code=500)
+@routes.get('/cache')
+async def scanPage(request):
+    print(cache)
+    return html(templates["scan.html"].render(cache=cache))
 
-@app.route("/scan")
-async def scan():
-    global cache
-    result_html = ""
-    n = "\n"
+@routes.get('/scan')
+async def scan(request):
     for result in await pyatv.scan(loop=asyncio.get_event_loop()):
         cache[result.identifier] = result
-        result_html += str(f"""
-    <div>
-        {result.name}
-        <br>
-        {str(result).replace(n, "<br>")}
-        <br>
-        <div>
-            <button onclick="begin_pair('{result.identifier}', this.parentElement)">Pair</button>
-        </div>
-        <hr>
-    </div>""")
-    if result_html:
-        return await render_template("scanner.html", results=result_html)
-    return await render_template("scanner.html", results="<h3>There are no Apple TVs on your network.</h3><a href='/'>Go Home</a>")
+    return html("<a href='/cache'><button>Go To Results</button></a>")
 
-app.run("0.0.0.0", 8080)
+# Run server with all the routes
+app = web.Application()
+app.add_routes(routes)
+web.run_app(app)
